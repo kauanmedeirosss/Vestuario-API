591b949839442062b11da3e9de02088b27897a14
lorimedeiros <lorimedeirosc@gmail.com>
2025-08-04 21:25:46 -0300

    upload inicial

---
+++ descrições.md
@@ -0,0 +1,12 @@
+# 1. Inicialização do projeto
+* Como foi configurado no spring initialzr:
+    - spring web
+    - Lombok
+![img.png](img.png)  
+* Como ficou o main + package:
+![img_1.png](img_1.png)
+
+## 1.1 Tecnologias:
+* IDE: IntelliJ
+* SGBD: MySQL
+* Requisições: Insomnia
---
+++ src/main/java/io/github/kauanmedeirosss/Vestuario/API/Application.java
@@ -0,0 +1,15 @@
+package io.github.kauanmedeirosss.Vestuario.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+// commit inicial do projeto com hello world usado para teste
